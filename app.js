549332e4f5b3cf20eef9ed8a9d0039d96d359ba0
Dushyant <dushyantgoyal28@gmail.com>
2020-05-19 13:39:45 +0530

    achievements and nodejs

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const path = require("path")
+
+const app = express();
+bodyParser.urlencoded({extended: true});
+
+
+app.use(express.static("public"));
+// app.use('/favicon.ico', express.static('public/images/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.listen(8080);{
+    console.log("Server running on Port 8080")
+}
