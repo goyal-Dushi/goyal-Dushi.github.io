d224ace84ec032c3c2f133ab72b35384d7181a6e
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-05-16 16:45:24 +0530

    nodemailer fixed

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const nodemailer = require("nodemailer");
 
 const app = express();
-bodyParser.urlencoded({extended: true});
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static(path.join(__dirname+"/public")));
 
 app.get('/', function(req, res){
