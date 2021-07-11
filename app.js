3928eda692a17995862f404b5b0568b717890018
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-07-11 20:12:12 +0530

    created all scss files for different sections

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/public")));
 
-app.get('/', function(req, res){
-    res.sendFile('/index.html');
-})
+app.get("/", function (req, res) {
+  res.sendFile("/index.html");
+});
 
-app.listen(process.env.PORT || 8080);{
-    console.log("Server running on Port 8080")
+app.get("/download", function (req, res) {
+  res.download(
+    __dirname + "/upload_folder/Resume.pdf",
+    "Dushyant_Resume.pdf",
+    (error) => {
+      console.log("Error :", error);
+    }
+  );
+});
+
+app.listen(process.env.PORT || 8080);
+{
+  console.log("Server running on Port 8080");
 }
