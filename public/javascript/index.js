0e0caff1b92c3dbf2d1bc04085200377c9aec5cb
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-11 20:51:51 +0530

    used event delegation to optimize btn click events

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -5,29 +5,35 @@ dateText.textContent = date.getFullYear();
 
 // NAVBAR TOGGLE
 const openNav = document.querySelector(".open-icon");
-const closeNav = document.querySelector(".close-icon");
 const responsiveNav = document.querySelector(".nav-content");
+const navWrapper = document.querySelector("#nav-wrapper");
+
+navWrapper.addEventListener("click", (e) => {
+  const navHeight = navWrapper.getBoundingClientRect().height;
+  console.log("Navheight: ", navHeight);
+  if (
+    e.target.className === "close-icon" ||
+    e.target.className === "close-line"
+  ) {
+    responsiveNav.style.right = "-100%";
+  } else if (e.target.className === "nav-link") {
+    const id = e.target.getAttribute("href").slice(1);
+    const page = document.getElementById(id);
+    const position = page.offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: position - navHeight,
+    });
+    responsiveNav.style.right = "-100%";
+  }
+});
 
 openNav.addEventListener("click", () => {
-  responsiveNav.style.right = "0";
+  responsiveNav.style.right = "0%";
 });
 
-closeNav.addEventListener("click", () => {
-  responsiveNav.style.right = "-100%";
-});
-
+// Btn Resume Download
 const resumeBtn = document.getElementById("download_resume");
 resumeBtn.addEventListener("click", () => {
   window.open("/download");
-});
-
-// NAV RESPONSE
-const navbar = document.getElementById("nav-wrapper");
-window.addEventListener("scroll", () => {
-  const navheight = navbar.getBoundingClientRect().height;
-  if (window.pageYOffset > navheight) {
-    navbar.classList.add("nav-onscroll-style");
-  } else {
-    navbar.classList.remove("nav-onscroll-style");
-  }
 });
